5c78cf6e883a5b6321686f45e8f0001ea647b084
Jonathan Zarate <zaratedev@gmail.com>
2018-09-12 15:54:05 -0500

    Events tigger, find

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -8,5 +8,28 @@ test('component has a name', () => {
 
 test('default data is correct', () => {
     const wrapper = mount(App);
-    expect(wrapper.vm.msg).toBe('Hello world!');
+    expect(wrapper.vm.msg).toBe('');
+});
+
+test('msg data is displayed', () => {
+    const wrapper = mount(App);
+    wrapper.setData({ msg: 'Hello world!'});
+    expect(wrapper.text()).toContain('Hello world!');
+});
+
+test('msg changes when button is clicked', () => {
+    const wrapper = mount(App);
+    const input = wrapper.find('input');
+    input.element.value = 'Updated text';
+    input.trigger('input');
+    expect(wrapper.html()).toContain('Updated text');
+});
+
+test('fullName computed is firstName and lastName', () => {
+    const wrapper = mount(App);
+    wrapper.setData({
+        firstName: 'John',
+        lastName: 'Doe'
+    });
+    expect(wrapper.vm.fullName).toBe('John Doe');
 });
