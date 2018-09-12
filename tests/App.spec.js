c1530eeed2d324177d966147ae2b2a1e969fe758
Jonathan Zarate <zaratedev@gmail.com>
2018-09-12 13:50:15 -0500

    My first test in Vue JS

---
+++ tests/App.spec.js
@@ -0,0 +1,12 @@
+import { mount } from 'vue-test-utils';
+import App from '@/App';
+
+test('component has a name', () => {
+    const wrapper = mount(App);
+    expect(wrapper.name()).toBe('App');
+});
+
+test('default data is correct', () => {
+    const wrapper = mount(App);
+    expect(wrapper.vm.msg).toBe('Hello world!');
+});
